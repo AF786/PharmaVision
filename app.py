7bbc601b3cbe047825a7d7389f4a9d4585cdf373
ark331 <arkhams312@gmail.com>
2025-04-17 21:25:38 +0530

    Update app.run configuration to use environment variable for port

--- app.py
+++ app.py
@@ -355,5 +355,6 @@ def detect_skin_disease():
         
 if __name__ == "__main__":
     # Run the Flask app
-    app.run("127.0.0.1", port=8080, debug=True)
-    
+    # app.run("127.0.0.1", port=8080, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
